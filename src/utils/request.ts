675aa2f427118fd38b3a33543f0ca361df478685
Remigiusz Mikolajewicz <r.mikolajewicz@interia.pl>
2023-04-26 16:35:39 +0200

    .

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -7,43 +7,54 @@ interface RequestType {
   url: string;
 }
 
-if (!API_KEY) throw new Error("No api key");
-
 export const requests = [
   {
     key: "fetchTrending",
-    url: `${BASE_URL}/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    url: API_KEY
+      ? `${BASE_URL}/trending/all/week?api_key=${API_KEY}&language=en-US`
+      : "",
   },
   {
     key: "fetchTopRated",
-    url: `${BASE_URL}/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    url: API_KEY
+      ? `${BASE_URL}/movie/top_rated?api_key=${API_KEY}&language=en-US`
+      : "",
   },
   {
     key: "fetchActionMovies",
-    url: `${BASE_URL}/discover/movie?api_key=${API_KEY}&language=en-US&with_genres=28`,
+    url: API_KEY
+      ? `${BASE_URL}/discover/movie?api_key=${API_KEY}&language=en-US&with_genres=28`
+      : "",
   },
   {
     key: "fetchComedyMovies",
-    url: `${BASE_URL}/discover/movie?api_key=${API_KEY}&language=en-US&with_genres=35`,
+    url: API_KEY
+      ? `${BASE_URL}/discover/movie?api_key=${API_KEY}&language=en-US&with_genres=35`
+      : "",
   },
   {
     key: "fetchHorrorMovies",
-    url: `${BASE_URL}/discover/movie?api_key=${API_KEY}&language=en-US&with_genres=27`,
+    url: API_KEY
+      ? `${BASE_URL}/discover/movie?api_key=${API_KEY}&language=en-US&with_genres=27`
+      : "",
   },
   {
     key: "fetchRomanceMovies",
-    url: `${BASE_URL}/discover/movie?api_key=${API_KEY}&language=en-US&with_genres=10749`,
+    url: API_KEY
+      ? `${BASE_URL}/discover/movie?api_key=${API_KEY}&language=en-US&with_genres=10749`
+      : "",
   },
   {
     key: "fetchDocumentaries",
-    url: `${BASE_URL}/discover/movie?api_key=${API_KEY}&language=en-US&with_genres=99`,
+    url: API_KEY
+      ? `${BASE_URL}/discover/movie?api_key=${API_KEY}&language=en-US&with_genres=99`
+      : "",
   },
 ];
 
 const fetchMultiple = async (
   requests: Array<RequestType>
 ): Promise<Array<ResponseDataType>> => {
-  if (!API_KEY) throw new Error("No Api Key");
   const responses = (await Promise.all(
     requests.map((request) => fetch(request.url).then((res) => res.json()))
   )) as ResponseDataType[];
